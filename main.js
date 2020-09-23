7743449edb51afba9b94c58850ba5ec5403ed69b
James Atyeo <jameswatyeo@gmail.com>
2020-09-23 16:48:04 +1000

    Added collision detection

--- main.js
+++ main.js
@@ -50,6 +50,21 @@ Ball.prototype.update = function() {
   this.y += this.velY;
 }
 
+Ball.prototype.collisionDetect = function() {
+  for (let j=0; j < balls.length; j++) {
+    if (!(this === balls[j])) {
+      // to understand this part, go to http://www.jeffreythompson.org/collision-detection/point-circle.php
+      const dx = this.x - balls[j].x;
+      const dy = this.y - balls[j].y;
+      const distance = Math.sqrt(dx*dx + dy*dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
+      }
+    }
+  }
+}
+
 let balls = [];
 
 while (balls.length < 25) {
@@ -66,9 +81,11 @@ function loop() {
   for (let i = 0; i < balls.length; i++) {
     balls[i].draw();
     balls[i].update();
+    balls[i].collisionDetect();
   }
 
   requestAnimationFrame(loop);
 }
 
 loop();
+
